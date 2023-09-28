dda1b0cb00cece1c0bbbaa901940adab70e250c3
DOCENTE-N412 <DOCENTE-N412@UCSP.AULAB>
2023-09-28 08:26:33 -0500

    ejemplos con arreglos

--- semana_6/arreglo2.cpp
+++ semana_6/arreglo2.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main() {
     int arr[4];
+    
     cout << "Ingrese los valores del arreglo: ";
     for (int i = 0; i < 4; i++) {
         cin >> arr[i];
--- semana_6/arreglo3.cpp
+++ semana_6/arreglo3.cpp
@@ -2,10 +2,27 @@
 
 using namespace std;
 
-long sumar(const int arr[], const int tam) {
+long sumarFor(const int arr[], const int tam) {
     long sum = 0;
     for(int i = 0; i < tam; sum += arr[i++]);
     return sum;
+}
+
+long sumarWhile(const int arr[], const int tam) {
+    long sum = 0;
+    int i = 0;
+    while(i < tam) {
+        sum += arr[i++];
+    }    
+    return sum;
+}
+
+// suma los elementos de forma RECURSIVO
+long sumarRec(const int arr[], const int tam) {
+    if(tam == 0)
+        return 0;
+    else
+        return arr[tam-1] + sumarRec(arr, tam-1);
 }
 
 void imprimir(const int arr[], const int tam) {
@@ -13,14 +30,23 @@ void imprimir(const int arr[], const int tam) {
     for (int i = 0; i < tam; i++) {
         cout << " " << arr[i];
     }
-    cout << "]" << endl;
+    cout << " ]" << endl;
+}
+
+void imprimirRec(const int arr[], const int tam, int i = 0) {
+    if(tam == 0)
+        return;
+    cout << arr[i] << " ";
+    imprimirRec(arr, tam-1, ++i);
 }
 
 int main() {
     const int tam = 4;
     int arr[tam] = {5, 6, 3, 1};
     
-    cout << "La suma es: " << sumar(arr, tam) << endl;    
+    cout << "La impresion es: " << sumarRec(arr, tam) << endl;
+
+    imprimirRec(arr, tam); 
     
     return 0;
 }
